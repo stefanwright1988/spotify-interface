1b5d600b6b070fe3f9a99588a6b26e5c4f7e8f73
Stefan Wright <stefanwright1988@gmail.com>
2022-09-26 12:50:20 +0100

    more changes

--- client/src/hooks/useAuth.tsx
+++ client/src/hooks/useAuth.tsx
@@ -1,23 +1,59 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const useAuth = (code) => {
+const useAuth = (code: string) => {
   const [accessToken, setAcessToken] = useState();
   const [refreshToken, setRefreshToken] = useState();
   const [expiresIn, setExpiresIn] = useState();
 
   useEffect(() => {
-    axios
-      .post(
-        "http://localhost:5001/spotify-react-ts-vite/us-central1/webApi/login",
-        {
-          code,
-        }
-      )
-      .then((res) => {
-        console.log(res.data);
-      });
+    console.log(code);
+    if (code) {
+      axios
+        .post(
+          "http://localhost:5001/spotify-react-ts-vite/us-central1/webApi/login",
+          {
+            code,
+          }
+        )
+        .then((res) => {
+          setAcessToken(res.data.accessToken);
+          setRefreshToken(res.data.refreshToken);
+          setExpiresIn(res.data.expiresIn);
+          window.history.pushState({}, "", "/");
+        })
+        .catch((err) => {
+          console.log(err);
+          //window.location.href = "/";
+        });
+    }
   }, [code]);
+
+  useEffect(() => {
+    if (!refreshToken || !expiresIn) return;
+    const timeout = setInterval(() => {
+      axios
+        .post(
+          "http://localhost:5001/spotify-react-ts-vite/us-central1/webApi/refresh",
+          {
+            refreshToken,
+          }
+        )
+        .then((res) => {
+          setAcessToken(res.data.accessToken);
+          setExpiresIn(res.data.expiresIn);
+          window.history.pushState({}, "", "/");
+        })
+        .catch((err) => {
+          console.log(err);
+          window.location.href = "/";
+        });
+    }, (expiresIn - 60) * 1000);
+
+    return () => clearInterval(timeout);
+  }, [refreshToken, expiresIn]);
+
+  return accessToken;
 };
 
 export default useAuth;
--- server/functions/src/index.ts
+++ server/functions/src/index.ts
@@ -8,6 +8,35 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken;
+
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://127.0.0.1:5173",
+    clientId: "8b82677d1fa148f4994da55224a4d540",
+    clientSecret: "03809aa8f5a24d09befd136ba4be0ce3",
+    refreshToken: refreshToken,
+  });
+
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      console.log("The access token has been refreshed!");
+      console.log(data.body);
+      res.json({
+        accessToken: data.body.access_token,
+        expiresIn: data.body.expires_in,
+      });
+      // Save the access token so that it's used in future calls
+      //spotifyApi.setAccessToken(data.body["access_token"]);
+    })
+    .catch((err) => {
+      console.log("Could not refresh access token", err);
+
+      res.status(400).send(err.message);
+    });
+});
+
 app.post("/login", (req, res) => {
   const authResCode = req.body.code;
 
@@ -27,7 +56,29 @@ app.post("/login", (req, res) => {
       });
     })
     .catch((err) => {
+      console.log(err);
       res.status(400).send(err.message);
+    });
+});
+
+app.get("/search", (req: any, res) => {
+  console.log(req);
+  const accessToken = req.query.accessToken;
+  const searchTerm = req.query.searchTerm;
+
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://127.0.0.1:5173",
+    clientId: "8b82677d1fa148f4994da55224a4d540",
+    clientSecret: "03809aa8f5a24d09befd136ba4be0ce3",
+  });
+  console.log(`accessToken is: ${accessToken}`);
+  spotifyApi.setAccessToken(accessToken);
+
+  spotifyApi
+    .searchTracks(searchTerm)
+    .then((response) => res.status(200).send(response))
+    .catch((err) => {
+      console.log(err);
     });
 });
 
